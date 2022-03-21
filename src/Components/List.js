c0b7ec137961e436951d45793e1c47c74360385e
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-03-21 15:53:52 +0530

    tool implemented

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const List = (props) => {
     const bookList = props.list.map((item) => {
-        return <li key={item.isbn}>{item.title}</li>;
+        return (
+            <div>
+                <li key={item.isbn}>
+                    <b>Book Name:</b> {item.title}
+                </li>
+                <h4>Author: {item.author}</h4>
+                <hr />
+            </div>
+        );
     });
     return <div>{bookList}</div>;
 };
